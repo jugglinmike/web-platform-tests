fd7d67b342f41409ad03f8bad34e9915c151a4d4
Mike Pennisi <mike@mikepennisi.com>
2017-01-25 15:49:41 -0500

    Complete initial set of tests

--- webdriver/navigation-get-title.py
+++ webdriver/navigation-get-title.py
@@ -57,14 +57,14 @@ class Result():
             # >    values:
             # >
             # >    "Content-Type"
-            # >       "application/json; charset=utf-8" 
+            # >       "application/json; charset=utf-8"
             # >    "cache-control"
-            # >       "no-cache" 
+            # >       "no-cache"
             assert response.getheader("Content-Type") == "application/json; charset=utf-8"
             assert response.getheader("cache-control") == "no-cache"
 
             # > 4. If data is not null, let response's body be a JSON Object with a
-            #      key value set to the JSON Serialization of data. 
+            #      key value set to the JSON Serialization of data.
             if self.body:
                 assert "value" in self.body
 
@@ -125,15 +125,7 @@ def command():
 def scommand(command, request):
     result = command("POST", "/session")
     assert result.status == 200
-    print 'MIKE'
-    print 'MIKE'
-    print 'MIKE'
-    print 'MIKE'
-    print 'MIKE', result.body
-    print 'MIKE'
-    print 'MIKE'
-    print 'MIKE'
-    print 'MIKE'
+    assert "sessionId" in result.body["value"]
     session_id = result.body["value"]["sessionId"]
 
     def scommand(*args, **kwargs):
@@ -143,6 +135,9 @@ def scommand(command, request):
 
     def delete_session():
         result = command("DELETE", "", session_id=session_id)
+        assert result.status == 200
+        assert result.body == None
+
     request.addfinalizer(delete_session)
 
     return scommand
@@ -172,13 +167,31 @@ def switch_to_inactive(scommand):
     return switch_to_inactive
 
 @pytest.fixture()
-def throwaway_dialog(scommand, request):
-    dialog_id = str(uuid4())
-    box = dict(dialog_type=None, dismissed_value=None)
+def switch_to_new_frame(scommand):
+    def switch_to_new_frame():
+        append = """
+            var frame = document.createElement('iframe');
+            document.body.appendChild(frame);
+            return frame;
+        """
+        result = scommand("POST", "/execute/sync", dict(script=append, args=[]))
+        assert result.status == 200
+        result = scommand("POST", "/frame", dict(id=result.body["value"]))
+        assert result.status == 200
+    return switch_to_new_frame
 
-    def throwaway_dialog(dialog_type, dismissed_value):
-        box["dialog_type"] = dialog_type
-        box["dismissed_value"] = dismissed_value
+@pytest.fixture()
+def create_dialog(scommand, request):
+    dismissed_values = {
+        "alert": None,
+        "prompt": None,
+        "confirm": False
+    }
+
+    def create_dialog(dialog_type, verify_dismissed):
+        dialog_id = str(uuid4())
+        dismissed_value = dismissed_values[dialog_type]
+
         spawn = """
             var done = arguments[0];
             window.__WEBDRIVER = "initial value {0}";
@@ -190,27 +203,28 @@ def throwaway_dialog(scommand, request):
 
         scommand('POST', '/execute/async', dict(script=spawn, args=[]))
 
-    def verify_dismissed():
-        result = scommand('GET', '/alert/text')
+        def verify():
+            result = scommand('GET', '/alert/text')
 
-        # If there were any existing dialogs prior to the creation of this
-        # fixture's dialog, then the "Get Alert Text" command will return
-        # successfully. In that case, the text must be different than that of
-        # this fixture's dialog.
-        try:
-            assert_error(result, 'no such alert')
-        except:
+            # If there were any existing dialogs prior to the creation of this
+            # fixture's dialog, then the "Get Alert Text" command will return
+            # successfully. In that case, the text must be different than that of
+            # this fixture's dialog.
+            try:
+                assert_error(result, 'no such alert')
+            except:
+                assert result.status == 200
+                assert result.body["value"] != "text {0}".format(dialog_id)
+
+            probe = 'return window.__WEBDRIVER;'
+            result = scommand('POST', '/execute/sync', dict(script=probe, args=[]))
             assert result.status == 200
-            assert result.body["value"] != "text {0}".format(dialog_id)
+            assert result.body["value"] == dismissed_value, "Dialog was dismissed, not accepted"
 
-        probe = 'return window.__WEBDRIVER;'
-        result = scommand('POST', '/execute/sync', dict(script=probe, args=[]))
-        assert result.status == 200
-        assert result.body["value"] == box["dismissed_value"], "Dialog was dismissed, not accepted"
+        if verify_dismissed:
+            request.addfinalizer(verify)
 
-    request.addfinalizer(verify_dismissed)
-
-    return throwaway_dialog
+    return create_dialog
 
 def assert_error(result, name):
     assert result.status == errors[name]
@@ -220,56 +234,42 @@ def assert_error(result, name):
         assert isinstance(result.body["value"]["stacktrace"], basestring)
 
 # 1. If the current top-level browsing context is no longer open, return error
-#    with error code no such window. 
+#    with error code no such window.
 def test_title_from_closed_context(scommand, switch_to_inactive):
     switch_to_inactive()
     result = scommand("GET", "/title")
     assert_error(result, "no such window")
 
-def test_title_from_top_context(scommand):
-    scommand("POST", "/url", dict(url=inline("<title>Foobar</title><h2>Hello</h2>")))
-    result = scommand("GET", "/title")
-    assert result.status == 200
-    assert result.body["value"] == "Foobar"
-
 # 2. Handle any user prompts and return its value if it is an error.
-def test_title_dismiss_confirm(scommand):
-    scommand("POST", "/url", dict(url=inline("<title>With `confirm` dialog</title><h2>Hello</h2>")))
-    scommand('POST', '/execute/sync', dict(script='return window.confirm();', args=[]))
+def test_title_dismiss_dialog_alert(scommand, create_dialog):
+    document = "<title>Dismiss `alert` dialog</title><h2>Hello</h2>"
+    scommand("POST", "/url", dict(url=inline(document)))
+    create_dialog(dialog_type='alert', verify_dismissed=True)
 
     result = scommand("GET", "/title")
     assert result.status == 200
-    assert result.body["value"] == "With `confirm` dialog"
+    assert result.body["value"] == "Dismiss `alert` dialog"
 
-def test_title_dismiss_dialog_alert(scommand, throwaway_dialog):
+def test_title_dismiss_dialog_prompt(scommand, create_dialog):
     document = "<title>Dismiss `prompt` dialog</title><h2>Hello</h2>"
     scommand("POST", "/url", dict(url=inline(document)))
-    throwaway_dialog(dialog_type='alert', dismissed_value=None)
+    create_dialog(dialog_type='prompt', verify_dismissed=True)
 
     result = scommand("GET", "/title")
     assert result.status == 200
     assert result.body["value"] == "Dismiss `prompt` dialog"
 
-def test_title_dismiss_dialog_prompt(scommand, throwaway_dialog):
-    document = "<title>Dismiss `prompt` dialog</title><h2>Hello</h2>"
+def test_title_dismiss_dialog_confirm(scommand, create_dialog):
+    document = "<title>Dismiss `confirm` dialog</title><h2>Hello</h2>"
     scommand("POST", "/url", dict(url=inline(document)))
-    throwaway_dialog(dialog_type='prompt', dismissed_value=None)
+    create_dialog(dialog_type='confirm', verify_dismissed=True)
 
     result = scommand("GET", "/title")
     assert result.status == 200
-    assert result.body["value"] == "Dismiss `prompt` dialog"
-
-def test_title_dismiss_dialog_confirm(scommand, throwaway_dialog):
-    document = "<title>Dismiss `prompt` dialog</title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
-    throwaway_dialog(dialog_type='confirm', dismissed_value=False)
-
-    result = scommand("GET", "/title")
-    assert result.status == 200
-    assert result.body["value"] == "Dismiss `prompt` dialog"
+    assert result.body["value"] == "Dismiss `confirm` dialog"
 
 def test_title_with_non_simple_dialog(scommand):
-    document = "<title>With simple dialogs</title><h2>Hello</h2>"
+    document = "<title>With non-simple dialog</title><h2>Hello</h2>"
     spawn = """
         var done = arguments[0];
         setTimeout(function() {
@@ -280,19 +280,32 @@ def test_title_with_non_simple_dialog(scommand):
     scommand("POST", "/url", dict(url=inline(document)))
     scommand('POST', '/execute/async', dict(script=spawn, args=[]))
 
-    time.sleep(2)
+    time.sleep(4)
+
     result = scommand("GET", "/title")
     assert_error(result, "unexpected alert open")
 
-def test_title_with_simple_dialogs(scommand):
-    document = "<title>With simple dialogs</title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
-    scommand('POST', '/execute/sync', dict(script='window.confirm("confirm");', args=[]))
-    scommand('POST', '/execute/sync', dict(script='window.alert("alert");', args=[]))
-    scommand('POST', '/execute/sync', dict(script='window.prompt("prompt");', args=[]))
-
+# 3. Let title be the initial value of the title IDL attribute of the current
+#    top-level browsing context's active document.
+def test_title_from_top_context(scommand):
+    scommand("POST", "/url", dict(url=inline("<title>Foobar</title><h2>Hello</h2>")))
     result = scommand("GET", "/title")
     assert result.status == 200
-    assert result.body["value"] == "With simple dialogs"
+    assert result.body["value"] == "Foobar"
 
-    # TODO: Verify that the dialog has been dismissed
+def test_title_without_element(scommand):
+    scommand("POST", "/url", dict(url=inline("<h2>Hello</h2>")))
+    result = scommand("GET", "/title")
+    assert result.status == 200
+    assert result.body["value"] == ""
+
+def test_title_from_frame(scommand, switch_to_new_frame):
+    scommand("POST", "/url", dict(url=inline("<title>Parent</title>parent")))
+
+    switch_to_new_frame()
+    switch_to_new_frame()
+
+    result = scommand("GET", "/title")
+
+    assert result.status == 200
+    assert result.body["value"] == "Parent"
