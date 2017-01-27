74fa6eb4ce82ce0b0603f6691d953b277fee1df0
Mike Pennisi <mike@mikepennisi.com>
2017-01-27 13:24:19 -0500

    Update to use patched version of `wdclient`

--- webdriver/conftest.py
+++ webdriver/conftest.py
@@ -3,72 +3,51 @@ from uuid import uuid4
 import pytest
 
 import util.wd_assert as wd_assert
-from util.request import request
 
 @pytest.fixture()
-def command(session):
-    host = session.transport.host
-    port = session.transport.port
-
-    def command(*args, **kwargs):
-        return request(host, port, *args, **kwargs)
-
-    return command
-
-@pytest.fixture()
-def scommand(command, session):
-    if not session.session_id:
-        session.start()
-
-    def scommand(*args, **kwargs):
-        if not "session_id" in kwargs:
-            kwargs["session_id"] = session.session_id
-        return command(*args, **kwargs)
-
-    return scommand
-
-@pytest.fixture()
-def switch_to_inactive(scommand):
+def switch_to_inactive(session):
     """Create a new browsing context, switch to that new context, and then
     delete the context."""
     def switch_to_inactive():
-        initial = scommand("GET", "/window/handles").body["value"]
+        initial = session.send_raw_command("GET", "window/handles").body["value"]
 
-        result = scommand("POST",
-                          "/execute/sync",
-                          dict(script="window.open();", args=[]))
+        result = session.send_raw_command("POST",
+                                      "execute/sync",
+                                      dict(script="window.open();", args=[]))
         assert result.status == 200
 
-        with_new = scommand("GET", "/window/handles").body["value"]
+        with_new = session.send_raw_command("GET", "window/handles").body["value"]
 
         assert len(initial) == len(with_new) - 1
 
         new_handle = (set(with_new) - set(initial)).pop()
 
-        result = scommand("POST", "/window", dict(handle=new_handle))
+        result = session.send_raw_command("POST", "window", dict(handle=new_handle))
 
         assert result.status == 200
 
-        scommand("DELETE", "/window")
+        session.send_raw_command("DELETE", "window")
 
     return switch_to_inactive
 
 @pytest.fixture()
-def switch_to_new_frame(scommand):
+def switch_to_new_frame(session):
     def switch_to_new_frame():
         append = """
             var frame = document.createElement('iframe');
             document.body.appendChild(frame);
             return frame;
         """
-        result = scommand("POST", "/execute/sync", dict(script=append, args=[]))
+        result = session.send_raw_command("POST",
+                                      "execute/sync",
+                                      dict(script=append, args=[]))
         assert result.status == 200
-        result = scommand("POST", "/frame", dict(id=result.body["value"]))
+        result = session.send_raw_command("POST", "frame", dict(id=result.body["value"]))
         assert result.status == 200
     return switch_to_new_frame
 
 @pytest.fixture()
-def create_dialog(scommand):
+def create_dialog(session):
     """Create a dialog (one of "alert", "prompt", or "confirm") and provide a
     function to validate that a the dialog has been "handled" (either accepted
     or dismissed) by returning some value."""
@@ -91,11 +70,13 @@ def create_dialog(scommand):
             }}, 0);
         """.format(dialog_id, dialog_type, dialog_text)
 
-        result = scommand('POST', '/execute/async', dict(script=spawn, args=[]))
+        result = session.send_raw_command('POST',
+                                      'execute/async',
+                                      dict(script=spawn, args=[]))
         assert result.status == 200
 
         def assert_handled(expected_value):
-            result = scommand('GET', '/alert/text')
+            result = session.send_raw_command('GET', 'alert/text')
 
             # If there were any existing dialogs prior to the creation of this
             # fixture's dialog, then the "Get Alert Text" command will return
@@ -108,7 +89,9 @@ def create_dialog(scommand):
                     "Dialog was not handled."
 
             probe = 'return window.__WEBDRIVER;'
-            result = scommand('POST', '/execute/sync', dict(script=probe, args=[]))
+            result = session.send_raw_command('POST',
+                                          'execute/sync',
+                                          dict(script=probe, args=[]))
 
             assert result.status == 200, \
                 "Unable to verify that dialog was handled with expected value"
--- webdriver/navigation-get-title.py
+++ webdriver/navigation-get-title.py
@@ -6,44 +6,50 @@ import util.wd_assert as wd_assert
 def inline(doc):
     return "data:text/html;charset=utf-8,%s" % urllib.quote(doc)
 
+@pytest.fixture
+def session(session):
+    if session.session_id is None:
+        session.start()
+    return session
+
 # 1. If the current top-level browsing context is no longer open, return error
 #    with error code no such window.
-def test_title_from_closed_context(scommand, switch_to_inactive):
+def test_title_from_closed_context(session, switch_to_inactive):
     switch_to_inactive()
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.error(result, "no such window")
 
 # 2. Handle any user prompts and return its value if it is an error.
-def test_title_dismiss_dialog_alert(scommand, create_dialog):
+def test_title_dismiss_dialog_alert(session, create_dialog):
     document = "<title>Dismisses`alert` dialog</title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
+    session.send_raw_command("POST", "url", dict(url=inline(document)))
     assert_handled = create_dialog('alert')
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.success(result, "Dismisses`alert` dialog")
 
     assert_handled(None)
 
-def test_title_dismiss_dialog_prompt(scommand, create_dialog):
+def test_title_dismiss_dialog_prompt(session, create_dialog):
     document = "<title>Dismisses`prompt` dialog</title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
+    session.send_raw_command("POST", "url", dict(url=inline(document)))
     assert_handled = create_dialog('prompt')
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.success(result, "Dismisses`prompt` dialog")
 
     assert_handled(None)
 
-def test_title_dismiss_dialog_confirm(scommand, create_dialog):
+def test_title_dismiss_dialog_confirm(session, create_dialog):
     document = "<title>Dismisses`confirm` dialog</title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
+    session.send_raw_command("POST", "url", dict(url=inline(document)))
     assert_handled = create_dialog('confirm')
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.success(result, "Dismisses`confirm` dialog")
 
@@ -54,7 +60,7 @@ def test_title_dismiss_dialog_confirm(scommand, create_dialog):
 # tests in this module that follow will fail spuriously.
 #
 # TODO: Research techniques for session isolation.
-#def test_title_with_non_simple_dialog(scommand):
+#def test_title_with_non_simple_dialog(session):
 #    document = "<title>With non-simple dialog</title><h2>Hello</h2>"
 #    spawn = """
 #        var done = arguments[0];
@@ -63,45 +69,45 @@ def test_title_dismiss_dialog_confirm(scommand, create_dialog):
 #            window.print();
 #        }, 0);
 #    """
-#    scommand("POST", "/url", dict(url=inline(document)))
-#    scommand('POST', '/execute/async', dict(script=spawn, args=[]))
+#    session.send_raw_command("POST", "url", dict(url=inline(document)))
+#    session.send_raw_command('POST', 'execute/async', dict(script=spawn, args=[]))
 #
-#    result = scommand("GET", "/title")
+#    result = session.send_raw_command("GET", "title")
 #    wd_assert.error(result, "unexpected alert open")
 
 # 3. Let title be the initial value of the title IDL attribute of the current
 #    top-level browsing context's active document.
-def test_title_from_top_context(scommand):
-    scommand("POST", "/url", dict(url=inline("<title>Foobar</title><h2>Hello</h2>")))
-    result = scommand("GET", "/title")
+def test_title_from_top_context(session):
+    session.send_raw_command("POST", "url", dict(url=inline("<title>Foobar</title><h2>Hello</h2>")))
+    result = session.send_raw_command("GET", "title")
     wd_assert.success(result, "Foobar")
 
-def test_title_without_element(scommand):
-    scommand("POST", "/url", dict(url=inline("<h2>Hello</h2>")))
-    result = scommand("GET", "/title")
+def test_title_without_element(session):
+    session.send_raw_command("POST", "url", dict(url=inline("<h2>Hello</h2>")))
+    result = session.send_raw_command("GET", "title")
     wd_assert.success(result, "")
 
-def test_title_after_modification(scommand):
+def test_title_after_modification(session):
     modify = "document.title = 'updated';"
-    scommand("POST", "/url", dict(url=inline("<title>Initial</title><h2>Hello</h2>")))
-    scommand("POST", "/execute/sync", dict(script=modify, args=[]))
+    session.send_raw_command("POST", "url", dict(url=inline("<title>Initial</title><h2>Hello</h2>")))
+    session.send_raw_command("POST", "execute/sync", dict(script=modify, args=[]))
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.success(result, "updated")
 
-def test_title_strip_and_collapse(scommand):
+def test_title_strip_and_collapse(session):
     document = "<title>   a b\tc\nd\t \n e\t\n </title><h2>Hello</h2>"
-    scommand("POST", "/url", dict(url=inline(document)))
-    result = scommand("GET", "/title")
+    session.send_raw_command("POST", "url", dict(url=inline(document)))
+    result = session.send_raw_command("GET", "title")
     wd_assert.success(result, "a b c d e")
 
-def test_title_from_frame(scommand, switch_to_new_frame):
-    scommand("POST", "/url", dict(url=inline("<title>Parent</title>parent")))
+def test_title_from_frame(session, switch_to_new_frame):
+    session.send_raw_command("POST", "url", dict(url=inline("<title>Parent</title>parent")))
 
     switch_to_new_frame()
     switch_to_new_frame()
 
-    result = scommand("GET", "/title")
+    result = session.send_raw_command("GET", "title")
 
     wd_assert.success(result, "Parent")
